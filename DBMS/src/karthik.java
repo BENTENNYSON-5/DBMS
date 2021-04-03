7e0ba6c031b1c91179fc382ec69618e5204e98f3
Supersold123 <akhillohithdoni@gmail.com>
2021-04-03 15:10:45 +0530

    commit1510

--- DBMS/src/karthik.java
+++ DBMS/src/karthik.java
@@ -1,14 +1,11 @@
 import javax.swing.*;
 public class karthik {
-<<<<<<< HEAD
-	//
 
-=======
     public static void main(String args[]) {
     	
-    	
+    	//changed by akhil at 1509
     	//kd
     	System.out.println("sub");
     }
->>>>>>> branch 'main' of https://github.com/BENTENNYSON-5/DBMS.git
+
 }
