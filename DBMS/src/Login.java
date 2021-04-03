737f655d009de361eacfecb5cb612ced8f5da782
BENTENNYSON-5 <mail2dskkarthik@gmail.com>
2021-04-03 15:42:23 +0530

    lollllll1542

---
+++ DBMS/src/Login.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.BorderLayout;
+public class Login extends JFrame{
+	public Login() {
+	}
+	public static void main(String[] args) {
+
+	}
+
+}
--- DBMS/src/dbms.java
+++
@@ -1,9 +0,0 @@
-
-public class dbms {
-	//akhil
-	public static void main(String args[]) {
-		System.out.println("add");
-	}
-
-
-}
