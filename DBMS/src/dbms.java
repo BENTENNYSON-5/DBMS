443815e862a5ef13e683c74287a40bd195f43410
BENTENNYSON-5 <mail2dskkarthik@gmail.com>
2021-04-03 13:44:22 +0530

    kothabangarulokam

--- DBMS/src/dbms.java
+++ DBMS/src/dbms.java
@@ -1,5 +1,8 @@
 
 public class dbms {
 	//karthik
+	public static void main(String args[]) {
+		System.out.println("add");
+	}
 
 }
--- DBMS/src/karthik.java
+++ DBMS/src/karthik.java
@@ -1,4 +1,9 @@
 import javax.swing.*;
 public class karthik {
-
+    public static void main(String args[]) {
+    	
+    	
+    	//kd
+    	System.out.println("sub");
+    }
 }
