03b452d91315ce071153f18152ebf782ca9e088f
BENTENNYSON-5 <mail2dskkarthik@gmail.com>
2021-04-03 12:50:20 +0530

    kotha

---
+++ DBMs530/src/Login.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.BorderLayout;
+public class Login extends JFrame{
+	public Login() {
+		
+		JScrollPane scrollPane = new JScrollPane();
+		getContentPane().add(scrollPane, BorderLayout.EAST);
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
