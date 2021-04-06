f4aa7614666adab0d3649fa02ffbe6d625b848bc
Supersold123 <akhillohithdoni@gmail.com>
2021-04-06 23:43:22 +0530

    Teacher_Profile_halfwork_by akhil at2343

--- DBMS/src/Teacher.java
+++ DBMS/src/Teacher.java
@@ -7,7 +7,10 @@ import javax.swing.border.EmptyBorder;
 import java.awt.Color;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 import javax.swing.JButton;
+import javax.swing.JTextPane;
 
 public class Teacher extends JFrame {
 
@@ -92,9 +95,113 @@ public class Teacher extends JFrame {
 		tea_contentPanel1.add(tea_contents);
 		
 		JPanel tea_MainPanel = new JPanel();
-		tea_MainPanel.setBackground(new Color(245, 245, 220));
+		tea_MainPanel.setBackground(new Color(0, 255, 255));
 		tea_MainPanel.setBounds(186, 65, 1080, 615);
 		contentPane.add(tea_MainPanel);
 		tea_MainPanel.setLayout(null);
+		
+		JLabel Profile_name_label = new JLabel("First Name");
+		Profile_name_label.setFont(new Font("Lato Semibold", Font.PLAIN, 20));
+		Profile_name_label.setBounds(80, 50, 170, 40);
+		tea_MainPanel.add(Profile_name_label);
+		
+		JLabel Profile_Lname_label = new JLabel("Last Name");
+		Profile_Lname_label.setFont(new Font("Lato Semibold", Font.PLAIN, 20));
+		Profile_Lname_label.setBounds(80, 90, 170, 40);
+		tea_MainPanel.add(Profile_Lname_label);
+		
+		JLabel profile_age_label = new JLabel("Age");
+		profile_age_label.setFont(new Font("Lato Semibold", Font.PLAIN, 20));
+		profile_age_label.setBounds(80, 130, 170, 40);
+		tea_MainPanel.add(profile_age_label);
+		
+		JLabel profile_id_label = new JLabel("I.D");
+		profile_id_label.setFont(new Font("Lato Semibold", Font.PLAIN, 20));
+		profile_id_label.setBounds(80, 170, 170, 40);
+		tea_MainPanel.add(profile_id_label);
+		
+		JLabel profile_course_label = new JLabel("Course");
+		profile_course_label.setFont(new Font("Lato Semibold", Font.PLAIN, 20));
+		profile_course_label.setBounds(80, 210, 170, 40);
+		tea_MainPanel.add(profile_course_label);
+		
+		JLabel profile_department_label = new JLabel("Department");
+		profile_department_label.setFont(new Font("Lato Semibold", Font.PLAIN, 20));
+		profile_department_label.setBounds(80, 250, 170, 40);
+		tea_MainPanel.add(profile_department_label);
+		
+		JLabel profile_salary_label = new JLabel("Salary");
+		profile_salary_label.setFont(new Font("Lato Semibold", Font.PLAIN, 20));
+		profile_salary_label.setBounds(80, 290, 170, 40);
+		tea_MainPanel.add(profile_salary_label);
+		
+		JLabel profile_dno_label = new JLabel("Door No.");
+		profile_dno_label.setFont(new Font("Lato Semibold", Font.PLAIN, 20));
+		profile_dno_label.setBounds(80, 330, 170, 40);
+		tea_MainPanel.add(profile_dno_label);
+		
+		JLabel profile_street_label = new JLabel("Street");
+		profile_street_label.setFont(new Font("Lato Semibold", Font.PLAIN, 20));
+		profile_street_label.setBounds(80, 370, 170, 40);
+		tea_MainPanel.add(profile_street_label);
+		
+		JLabel profile_city_label = new JLabel("City");
+		profile_city_label.setFont(new Font("Lato Semibold", Font.PLAIN, 20));
+		profile_city_label.setBounds(80, 410, 170, 40);
+		tea_MainPanel.add(profile_city_label);
+		
+		JTextPane profile_name_pane = new JTextPane();
+		profile_name_pane.setText("TNAME");
+		profile_name_pane.setBounds(250, 57, 624, 20);
+		tea_MainPanel.add(profile_name_pane);
+		
+		JTextPane Profile_Lname_pane = new JTextPane();
+		Profile_Lname_pane.setText("TLNAME");
+		Profile_Lname_pane.setBounds(250, 97, 624, 20);
+		tea_MainPanel.add(Profile_Lname_pane);
+		
+		JTextPane profile_age_pane = new JTextPane();
+		profile_age_pane.setText("TAGE");
+		profile_age_pane.setBounds(250, 137, 624, 20);
+		tea_MainPanel.add(profile_age_pane);
+		
+		JTextPane profile_id_pane = new JTextPane();
+		profile_id_pane.setText("TID");
+		profile_id_pane.setBounds(250, 177, 624, 20);
+		tea_MainPanel.add(profile_id_pane);
+		
+		JTextPane profile_course_pane = new JTextPane();
+		profile_course_pane.setText("Course name");
+		profile_course_pane.setBounds(250, 217, 624, 20);
+		tea_MainPanel.add(profile_course_pane);
+		
+		JTextPane profile_department_pane = new JTextPane();
+		profile_department_pane.setText("DEpartment Name");
+		profile_department_pane.setBounds(250, 257, 624, 20);
+		tea_MainPanel.add(profile_department_pane);
+		
+		JTextPane profile_salary_pane = new JTextPane();
+		profile_salary_pane.setText("TSAL");
+		profile_salary_pane.setBounds(250, 297, 624, 20);
+		tea_MainPanel.add(profile_salary_pane);
+		
+		JTextPane profile_dno_pane = new JTextPane();
+		profile_dno_pane.setText("TDNO");
+		profile_dno_pane.setBounds(250, 337, 624, 20);
+		tea_MainPanel.add(profile_dno_pane);
+		
+		JTextPane profile_street_pane = new JTextPane();
+		profile_street_pane.setText("TSTR");
+		profile_street_pane.setBounds(250, 377, 624, 20);
+		tea_MainPanel.add(profile_street_pane);
+		
+		JTextPane profile_city_pane = new JTextPane();
+		profile_city_pane.setText("TCITY");
+		profile_city_pane.setBounds(250, 417, 624, 20);
+		tea_MainPanel.add(profile_city_pane);
+		
+		
+		//on clicking profile
+		
 	}
 }
