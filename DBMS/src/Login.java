52470151e902c89d396263e902b89d29ba020da6
shivaharshith <shiva.harshith2000@gmail.com>
2021-04-08 22:12:11 +0530

    student page2

--- DBMS/src/Login.java
+++ DBMS/src/Login.java
@@ -3,45 +3,45 @@ import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 class Login extends JFrame{
-		JLabel l1,l2,l3,l4;
-		JTextField t1,t4;
-		JPasswordField t2;
-		JButton b1;
+		JLabel login_page_label,userid_label,password_label,l4;
+		JTextField userid,t4;
+		JPasswordField password;
+		JButton login_button2;
 		JComboBox comboBox;
 		Login(){
 			Font f=new Font("Arial",Font.BOLD,24);
-			l1=new JLabel("Login Page");
-			l1.setFont(f);
-			l2=new JLabel("UserName");
-			t1=new JTextField();
-			l3=new JLabel("Password");
-			t2=new JPasswordField();
-			b1=new JButton("Login");
-			l1.setBounds(70,35,200,40);
-			l2.setBounds(70,100,100,20);
-			t1.setBounds(70,120,200,30); 
-			l3.setBounds(70,170,100,20);
-			t2.setBounds(70,190,200,30);
-			b1.setBounds(170,309,100,30);
-			getContentPane().add(l1);
-			getContentPane().add(l2);
-			getContentPane().add(t1);
-			getContentPane().add(l3);
-			getContentPane().add(t2);
-			getContentPane().add(b1);
+			login_page_label=new JLabel("Login Page");
+			login_page_label.setFont(f);
+			userid_label=new JLabel("User Id");
+			userid=new JTextField();
+			password_label=new JLabel("Password");
+			password=new JPasswordField();
+			login_button2=new JButton("Login");
+			login_page_label.setBounds(70,35,200,40);
+			userid_label.setBounds(70,100,100,20);
+			userid.setBounds(70,120,200,30); 
+			password_label.setBounds(70,170,100,20);
+			password.setBounds(70,190,200,30);
+			login_button2.setBounds(170,309,100,30);
+			getContentPane().add(login_page_label);
+			getContentPane().add(userid_label);
+			getContentPane().add(userid);
+			getContentPane().add(password_label);
+			getContentPane().add(password);
+			getContentPane().add(login_button2);
 			getContentPane().setLayout(null);
 			String[] designation= {"Teacher","Student","Administration","Teaching Assistant" };
 			comboBox = new JComboBox(designation);
 			comboBox.setBounds(70, 259, 200, 30);
 			getContentPane().add(comboBox);
 			
-			JLabel lblNewLabel = new JLabel("Designation");
-			lblNewLabel.setBounds(70, 241, 76, 14);
-			getContentPane().add(lblNewLabel);
+			JLabel designation_label = new JLabel("Designation");
+			designation_label.setBounds(70, 241, 76, 14);
+			getContentPane().add(designation_label);
 			setVisible(true);
 			setSize(400,401);
 			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			b1.addActionListener(new ActionListener(){
+			login_button2.addActionListener(new ActionListener(){
 				public void actionPerformed(ActionEvent ae){
 					
 				}
