bd71e2bd3d8425164e20842c1939be3230fa1f6c
Supersold123 <akhillohithdoni@gmail.com>
2021-04-08 13:41:27 +0530

    teacher and ta updated by A at 1341

--- DBMS/src/Teacher.java
+++ DBMS/src/Teacher.java
@@ -17,9 +17,9 @@ import java.awt.CardLayout;
 
 public class Teacher extends JFrame {
 
-	private JPanel contentPane;
+	private JPanel tea_contentPane;
 	private JTable timetable_table;
-	private JTable table;
+	private JTable coursedetails_studentslist_table;
 
 	/**
 	 * Launch the application.
@@ -43,35 +43,35 @@ public class Teacher extends JFrame {
 	public Teacher() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1280, 720);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		tea_contentPane = new JPanel();
+		tea_contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(tea_contentPane);
+		tea_contentPane.setLayout(null);
 		
-		JLayeredPane layeredPane = new JLayeredPane();
-		layeredPane.setBounds(186, 65, 1080, 615);
-		contentPane.add(layeredPane);
-		layeredPane.setLayout(new CardLayout(0, 0));
+		JLayeredPane tea_layeredPane = new JLayeredPane();
+		tea_layeredPane.setBounds(186, 65, 1080, 615);
+		tea_contentPane.add(tea_layeredPane);
+		tea_layeredPane.setLayout(new CardLayout(0, 0));
 		
-		JPanel tea_panel_1 = new JPanel();
-		tea_panel_1.setBackground(Color.CYAN);
-		layeredPane.add(tea_panel_1, "name_332569207428300");
+		JPanel tea_profile_panel = new JPanel();
+		tea_profile_panel.setBackground(Color.CYAN);
+		tea_layeredPane.add(tea_profile_panel, "name_332569207428300");
 		
-		JPanel tea_panel_2 = new JPanel();
-		tea_panel_2.setBackground(Color.GREEN);
-		layeredPane.add(tea_panel_2, "name_332525239451000");
-		tea_panel_2.setLayout(null);
+		JPanel tea_timetable_panel = new JPanel();
+		tea_timetable_panel.setBackground(Color.GREEN);
+		tea_layeredPane.add(tea_timetable_panel, "name_332525239451000");
+		tea_timetable_panel.setLayout(null);
 		
-		JPanel tea_panel_3 = new JPanel();
-		tea_panel_3.setBackground(Color.ORANGE);
-		layeredPane.add(tea_panel_3, "name_332625872803200");
-		tea_panel_3.setLayout(null);
-		tea_panel_1.setLayout(null);
+		JPanel tea_coursedetails_panel = new JPanel();
+		tea_coursedetails_panel.setBackground(Color.ORANGE);
+		tea_layeredPane.add(tea_coursedetails_panel, "name_332625872803200");
+		tea_coursedetails_panel.setLayout(null);
+		tea_profile_panel.setLayout(null);
 		
 		JPanel tea_headingPanel = new JPanel();
 		tea_headingPanel.setBackground(Color.YELLOW);
 		tea_headingPanel.setBounds(0, 0, 1266, 65);
-		contentPane.add(tea_headingPanel);
+		tea_contentPane.add(tea_headingPanel);
 		tea_headingPanel.setLayout(null);
 		
 		JLabel tea_heading = new JLabel("Teacher's Management System ");
@@ -83,46 +83,46 @@ public class Teacher extends JFrame {
 		JPanel tea_contentPanel2 = new JPanel();
 		tea_contentPanel2.setBackground(new Color(127, 255, 212));
 		tea_contentPanel2.setBounds(0, 110, 186, 570);
-		contentPane.add(tea_contentPanel2);
+		tea_contentPane.add(tea_contentPanel2);
 		tea_contentPanel2.setLayout(null);
 		
-		JButton tea_profile = new JButton("Profile");
-		tea_profile.setFont(new Font("Tahoma", Font.PLAIN, 30));
-		tea_profile.setForeground(new Color(0, 0, 0));
-		tea_profile.setBounds(0, 0, 186, 60);
-		tea_contentPanel2.add(tea_profile);
-		tea_profile.addActionListener(new ActionListener(){  
+		JButton tea_profile_button = new JButton("Profile");
+		tea_profile_button.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		tea_profile_button.setForeground(new Color(0, 0, 0));
+		tea_profile_button.setBounds(0, 0, 186, 60);
+		tea_contentPanel2.add(tea_profile_button);
+		tea_profile_button.addActionListener(new ActionListener(){  
 		    public void actionPerformed(ActionEvent e){
-		        layeredPane.removeAll();
-		        layeredPane.add(tea_panel_1);
-		        layeredPane.repaint();
-		        layeredPane.revalidate();    
+		        tea_layeredPane.removeAll();
+		        tea_layeredPane.add(tea_profile_panel);
+		        tea_layeredPane.repaint();
+		        tea_layeredPane.revalidate();    
 		    }  
 		    });
 		
-		JButton tea_TimeTable = new JButton("Time Table");
-		tea_TimeTable.setFont(new Font("Tahoma", Font.PLAIN, 30));
-		tea_TimeTable.setBounds(0, 60, 186, 60);
-		tea_contentPanel2.add(tea_TimeTable);
-		tea_TimeTable.addActionListener(new ActionListener(){  
+		JButton tea_TimeTable_button = new JButton("Time Table");
+		tea_TimeTable_button.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		tea_TimeTable_button.setBounds(0, 60, 186, 60);
+		tea_contentPanel2.add(tea_TimeTable_button);
+		tea_TimeTable_button.addActionListener(new ActionListener(){  
 		    public void actionPerformed(ActionEvent e){
-		        layeredPane.removeAll();
-		        layeredPane.add(tea_panel_2);
-		        layeredPane.repaint();
-		        layeredPane.revalidate();    
+		        tea_layeredPane.removeAll();
+		        tea_layeredPane.add(tea_timetable_panel);
+		        tea_layeredPane.repaint();
+		        tea_layeredPane.revalidate();    
 		    }  
 		    });
 		
-		JButton tea_CourseDetails = new JButton("Course Details");
-		tea_CourseDetails.setFont(new Font("Tahoma", Font.PLAIN, 24));
-		tea_CourseDetails.setBounds(0, 120, 186, 60);
-		tea_contentPanel2.add(tea_CourseDetails);
-		tea_CourseDetails.addActionListener(new ActionListener(){  
+		JButton tea_CourseDetails_button = new JButton("Course Details");
+		tea_CourseDetails_button.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		tea_CourseDetails_button.setBounds(0, 120, 186, 60);
+		tea_contentPanel2.add(tea_CourseDetails_button);
+		tea_CourseDetails_button.addActionListener(new ActionListener(){  
 		    public void actionPerformed(ActionEvent e){
-		        layeredPane.removeAll();
-		        layeredPane.add(tea_panel_3);
-		        layeredPane.repaint();
-		        layeredPane.revalidate();    
+		        tea_layeredPane.removeAll();
+		        tea_layeredPane.add(tea_coursedetails_panel);
+		        tea_layeredPane.repaint();
+		        tea_layeredPane.revalidate();    
 		    }  
 		    });
 		
@@ -134,7 +134,7 @@ public class Teacher extends JFrame {
 		JPanel tea_contentPanel1 = new JPanel();
 		tea_contentPanel1.setBackground(new Color(127, 255, 0));
 		tea_contentPanel1.setBounds(0, 65, 186, 45);
-		contentPane.add(tea_contentPanel1);
+		tea_contentPane.add(tea_contentPanel1);
 		tea_contentPanel1.setLayout(null);
 		
 		JLabel tea_contents = new JLabel("Contents");
@@ -149,141 +149,141 @@ public class Teacher extends JFrame {
 		JLabel Profile_name_label = new JLabel("First Name");
 		Profile_name_label.setFont(new Font("Lato Semibold", Font.PLAIN, 20));
 		Profile_name_label.setBounds(80, 50, 170, 40);
-		tea_panel_1.add(Profile_name_label);
+		tea_profile_panel.add(Profile_name_label);
 		
 		JLabel Profile_Lname_label = new JLabel("Last Name");
 		Profile_Lname_label.setFont(new Font("Lato Semibold", Font.PLAIN, 20));
 		Profile_Lname_label.setBounds(80, 90, 170, 40);
-		tea_panel_1.add(Profile_Lname_label);
+		tea_profile_panel.add(Profile_Lname_label);
 		
 		JLabel profile_age_label = new JLabel("Age");
 		profile_age_label.setFont(new Font("Lato Semibold", Font.PLAIN, 20));
 		profile_age_label.setBounds(80, 130, 170, 40);
-		tea_panel_1.add(profile_age_label);
+		tea_profile_panel.add(profile_age_label);
 		
 		JLabel profile_id_label = new JLabel("I.D");
 		profile_id_label.setFont(new Font("Lato Semibold", Font.PLAIN, 20));
 		profile_id_label.setBounds(80, 170, 170, 40);
-		tea_panel_1.add(profile_id_label);
+		tea_profile_panel.add(profile_id_label);
 		
 		JLabel profile_course_label = new JLabel("Course");
 		profile_course_label.setFont(new Font("Lato Semibold", Font.PLAIN, 20));
 		profile_course_label.setBounds(80, 210, 170, 40);
-		tea_panel_1.add(profile_course_label);
+		tea_profile_panel.add(profile_course_label);
 		
 		JLabel profile_department_label = new JLabel("Department");
 		profile_department_label.setFont(new Font("Lato Semibold", Font.PLAIN, 20));
 		profile_department_label.setBounds(80, 250, 170, 40);
-		tea_panel_1.add(profile_department_label);
+		tea_profile_panel.add(profile_department_label);
 		
 		JLabel profile_salary_label = new JLabel("Salary");
 		profile_salary_label.setFont(new Font("Lato Semibold", Font.PLAIN, 20));
 		profile_salary_label.setBounds(80, 290, 170, 40);
-		tea_panel_1.add(profile_salary_label);
+		tea_profile_panel.add(profile_salary_label);
 		
 		JLabel profile_dno_label = new JLabel("Door No.");
 		profile_dno_label.setFont(new Font("Lato Semibold", Font.PLAIN, 20));
 		profile_dno_label.setBounds(80, 330, 170, 40);
-		tea_panel_1.add(profile_dno_label);
+		tea_profile_panel.add(profile_dno_label);
 		
 		JLabel profile_street_label = new JLabel("Street");
 		profile_street_label.setFont(new Font("Lato Semibold", Font.PLAIN, 20));
 		profile_street_label.setBounds(80, 370, 170, 40);
-		tea_panel_1.add(profile_street_label);
+		tea_profile_panel.add(profile_street_label);
 		
 		JLabel profile_city_label = new JLabel("City");
 		profile_city_label.setFont(new Font("Lato Semibold", Font.PLAIN, 20));
 		profile_city_label.setBounds(80, 410, 170, 40);
-		tea_panel_1.add(profile_city_label);
+		tea_profile_panel.add(profile_city_label);
 		
 		JTextPane profile_name_pane = new JTextPane();
 		profile_name_pane.setText("TNAME");
 		profile_name_pane.setBounds(250, 57, 624, 20);
-		tea_panel_1.add(profile_name_pane);
+		tea_profile_panel.add(profile_name_pane);
 		profile_name_pane.setEditable(false);
 		
 		JTextPane Profile_Lname_pane = new JTextPane();
 		Profile_Lname_pane.setText("TLNAME");
 		Profile_Lname_pane.setBounds(250, 97, 624, 20);
-		tea_panel_1.add(Profile_Lname_pane);
+		tea_profile_panel.add(Profile_Lname_pane);
 		Profile_Lname_pane.setEditable(false);
 		
 		JTextPane profile_age_pane = new JTextPane();
 		profile_age_pane.setText("TAGE");
 		profile_age_pane.setBounds(250, 137, 624, 20);
-		tea_panel_1.add(profile_age_pane);
+		tea_profile_panel.add(profile_age_pane);
 		profile_age_pane.setEditable(false);
 		
 		JTextPane profile_id_pane = new JTextPane();
 		profile_id_pane.setText("TID");
 		profile_id_pane.setBounds(250, 177, 624, 20);
-		tea_panel_1.add(profile_id_pane);
+		tea_profile_panel.add(profile_id_pane);
 		profile_id_pane.setEditable(false);
 		
 		JTextPane profile_course_pane = new JTextPane();
 		profile_course_pane.setText("Course name");
 		profile_course_pane.setBounds(250, 217, 624, 20);
-		tea_panel_1.add(profile_course_pane);
+		tea_profile_panel.add(profile_course_pane);
 		profile_course_pane.setEditable(false);
 		
 		JTextPane profile_department_pane = new JTextPane();
 		profile_department_pane.setText("DEpartment Name");
 		profile_department_pane.setBounds(250, 257, 624, 20);
-		tea_panel_1.add(profile_department_pane);
+		tea_profile_panel.add(profile_department_pane);
 		profile_department_pane.setEditable(false);
 		
 		JTextPane profile_salary_pane = new JTextPane();
 		profile_salary_pane.setText("TSAL");
 		profile_salary_pane.setBounds(250, 297, 624, 20);
-		tea_panel_1.add(profile_salary_pane);
+		tea_profile_panel.add(profile_salary_pane);
 		profile_salary_pane.setEditable(false);
 		
 		JTextPane profile_dno_pane = new JTextPane();
 		profile_dno_pane.setText("TDNO");
 		profile_dno_pane.setBounds(250, 337, 624, 20);
-		tea_panel_1.add(profile_dno_pane);
+		tea_profile_panel.add(profile_dno_pane);
 		profile_dno_pane.setEditable(false);
 		
 		JTextPane profile_street_pane = new JTextPane();
 		profile_street_pane.setText("TSTR");
 		profile_street_pane.setBounds(250, 377, 624, 20);
-		tea_panel_1.add(profile_street_pane);
+		tea_profile_panel.add(profile_street_pane);
 		profile_street_pane.setEditable(false);
 		
 		JTextPane profile_city_pane = new JTextPane();
 		profile_city_pane.setText("TCITY");
 		profile_city_pane.setBounds(250, 417, 624, 20);
-		tea_panel_1.add(profile_city_pane);
+		tea_profile_panel.add(profile_city_pane);
 		profile_city_pane.setEditable(false);
 		
 		timetable_table = new JTable();
 		timetable_table.setBounds(69, 52, 944, 509);
-		tea_panel_2.add(timetable_table);
+		tea_timetable_panel.add(timetable_table);
 		
 		JLabel timetable_label = new JLabel("Time Table");
 		timetable_label.setFont(new Font("Tahoma", Font.BOLD, 30));
 		timetable_label.setBounds(438, 10, 166, 37);
-		tea_panel_2.add(timetable_label);
+		tea_timetable_panel.add(timetable_label);
 		
-		JLabel course_name_label = new JLabel("Course");
-		course_name_label.setBounds(69, 26, 170, 40);
-		course_name_label.setFont(new Font("Lato Semibold", Font.PLAIN, 20));
-		tea_panel_3.add(course_name_label);
+		JLabel coursedetails_coursename_label = new JLabel("Course");
+		coursedetails_coursename_label.setBounds(69, 26, 170, 40);
+		coursedetails_coursename_label.setFont(new Font("Lato Semibold", Font.PLAIN, 20));
+		tea_coursedetails_panel.add(coursedetails_coursename_label);
 		
-		JTextPane course_name_pane = new JTextPane();
-		course_name_pane.setBounds(282, 34, 624, 20);
-		course_name_pane.setText("Course name");
-		tea_panel_3.add(course_name_pane);
-		course_name_pane.setEditable(false);
+		JTextPane coursedetails_coursename_pane = new JTextPane();
+		coursedetails_coursename_pane.setBounds(282, 34, 624, 20);
+		coursedetails_coursename_pane.setText("Course name");
+		tea_coursedetails_panel.add(coursedetails_coursename_pane);
+		coursedetails_coursename_pane.setEditable(false);
 		
-		JLabel tea3_studentsenrolled_label = new JLabel("Students Enrolled:");
-		tea3_studentsenrolled_label.setFont(new Font("Lato Semibold", Font.PLAIN, 20));
-		tea3_studentsenrolled_label.setBounds(69, 65, 170, 40);
-		tea_panel_3.add(tea3_studentsenrolled_label);
+		JLabel coursedetails_studentsenrolled_label = new JLabel("Students Enrolled:");
+		coursedetails_studentsenrolled_label.setFont(new Font("Lato Semibold", Font.PLAIN, 20));
+		coursedetails_studentsenrolled_label.setBounds(69, 65, 170, 40);
+		tea_coursedetails_panel.add(coursedetails_studentsenrolled_label);
 		
-		table = new JTable();
-		table.setBounds(69, 131, 944, 441);
-		tea_panel_3.add(table);
+		coursedetails_studentslist_table = new JTable();
+		coursedetails_studentslist_table.setBounds(69, 131, 944, 441);
+		tea_coursedetails_panel.add(coursedetails_studentslist_table);
 		
 		
 		
---
+++ DBMS/src/ta.java
@@ -0,0 +1,218 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextPane;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLayeredPane;
+import java.awt.CardLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class ta extends JFrame {
+
+	private JPanel ta_contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ta frame = new ta();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ta() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1280, 720);
+		ta_contentPane = new JPanel();
+		ta_contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(ta_contentPane);
+		ta_contentPane.setLayout(null);
+		
+		JLayeredPane ta_layeredPane = new JLayeredPane();
+		ta_layeredPane.setBounds(186, 65, 1080, 615);
+		ta_contentPane.add(ta_layeredPane);
+		ta_layeredPane.setLayout(new CardLayout(0, 0));
+		
+		JPanel ta_profile_panel = new JPanel();
+		ta_profile_panel.setBackground(Color.CYAN);
+		ta_layeredPane.add(ta_profile_panel, "name_349220811387700");
+		ta_profile_panel.setLayout(null);
+		
+		JPanel ta_studentsmarks_panel = new JPanel();
+		ta_studentsmarks_panel.setBackground(Color.ORANGE);
+		ta_layeredPane.add(ta_studentsmarks_panel, "name_349237760040100");
+		ta_studentsmarks_panel.setLayout(null);
+		
+		JPanel ta_headingPanel = new JPanel();
+		ta_headingPanel.setBackground(Color.YELLOW);
+		ta_headingPanel.setBounds(0, 0, 1266, 65);
+		ta_contentPane.add(ta_headingPanel);
+		ta_headingPanel.setLayout(null);
+		
+		JLabel ta_heading = new JLabel("Teaching Assistant's Management System ");
+		ta_heading.setBounds(255, 10, 823, 46);
+		ta_heading.setForeground(Color.RED);
+		ta_heading.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 38));
+		ta_headingPanel.add(ta_heading);
+		
+		JPanel ta_contentPanel2 = new JPanel();
+		ta_contentPanel2.setBackground(new Color(127, 255, 212));
+		ta_contentPanel2.setBounds(0, 110, 186, 570);
+		ta_contentPane.add(ta_contentPanel2);
+		ta_contentPanel2.setLayout(null);
+		
+		JButton ta_profile_button = new JButton("Profile");
+		ta_profile_button.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		ta_profile_button.setBounds(0, 0, 186, 60);
+		ta_contentPanel2.add(ta_profile_button);
+		ta_profile_button.addActionListener(new ActionListener(){  
+		    public void actionPerformed(ActionEvent e){
+		        ta_layeredPane.removeAll();
+		        ta_layeredPane.add(ta_profile_panel);
+		        ta_layeredPane.repaint();
+		        ta_layeredPane.revalidate();    
+		    }  
+		    });
+		
+		JButton ta_studentsmarks_button = new JButton("Student's Marks");
+		ta_studentsmarks_button.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		ta_studentsmarks_button.setBounds(0, 60, 186, 60);
+		ta_contentPanel2.add(ta_studentsmarks_button);
+		ta_studentsmarks_button.addActionListener(new ActionListener(){  
+		    public void actionPerformed(ActionEvent e){
+		        ta_layeredPane.removeAll();
+		        ta_layeredPane.add(ta_studentsmarks_panel);
+		        ta_layeredPane.repaint();
+		        ta_layeredPane.revalidate();    
+		    }  
+		    });
+		
+		JButton ta_LogOut = new JButton("LOG OUT");
+		ta_LogOut.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		ta_LogOut.setBounds(0, 510, 186, 60);
+		ta_contentPanel2.add(ta_LogOut);
+		
+		JPanel ta_contentPanel1 = new JPanel();
+		ta_contentPanel1.setBackground(new Color(127, 255, 0));
+		ta_contentPanel1.setBounds(0, 65, 186, 45);
+		ta_contentPane.add(ta_contentPanel1);
+		ta_contentPanel1.setLayout(null);
+		
+		JLabel ta_contents = new JLabel("Contents");
+		ta_contents.setBounds(34, 10, 113, 25);
+		ta_contents.setFont(new Font("Rockwell Extra Bold", Font.PLAIN, 21));
+		ta_contentPanel1.add(ta_contents);
+				
+		
+		JLabel taname_label = new JLabel("Name");
+		taname_label.setFont(new Font("Lato Semibold", Font.PLAIN, 20));
+		taname_label.setBounds(80, 50, 170, 40);
+		ta_profile_panel.add(taname_label);
+		
+		JLabel taage_label = new JLabel("Age");
+		taage_label.setFont(new Font("Lato Semibold", Font.PLAIN, 20));
+		taage_label.setBounds(80, 90, 170, 40);
+		ta_profile_panel.add(taage_label);
+		
+		JLabel tacourse_label = new JLabel("Course assigned");
+		tacourse_label.setFont(new Font("Lato Semibold", Font.PLAIN, 20));
+		tacourse_label.setBounds(80, 130, 170, 40);
+		ta_profile_panel.add(tacourse_label);
+		
+		JLabel tasalary_label = new JLabel("Salary");
+		tasalary_label.setFont(new Font("Lato Semibold", Font.PLAIN, 20));
+		tasalary_label.setBounds(80, 170, 170, 40);
+		ta_profile_panel.add(tasalary_label);
+		
+		JTextPane taname_pane = new JTextPane();
+		taname_pane.setText("TANAME");
+		taname_pane.setBounds(250, 57, 624, 20);
+		ta_profile_panel.add(taname_pane);
+		taname_pane.setEditable(false);
+		
+		JTextPane taage_pane = new JTextPane();
+		taage_pane.setText("TAAGE");
+		taage_pane.setBounds(250, 97, 624, 20);
+		ta_profile_panel.add(taage_pane);
+		taage_pane.setEditable(false);
+		
+		JTextPane tacourse_pane = new JTextPane();
+		tacourse_pane.setText("Course name");
+		tacourse_pane.setBounds(250, 137, 624, 20);
+		ta_profile_panel.add(tacourse_pane);
+		tacourse_pane.setEditable(false);
+		
+		JTextPane tasalary_pane = new JTextPane();
+		tasalary_pane.setText("TASAL");
+		tasalary_pane.setBounds(250, 177, 624, 20);
+		ta_profile_panel.add(tasalary_pane);
+		tasalary_pane.setEditable(false);
+		
+		JLabel ta2_course_label = new JLabel("Course");
+		ta2_course_label.setForeground(Color.BLACK);
+		ta2_course_label.setBackground(Color.WHITE);
+		ta2_course_label.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		ta2_course_label.setBounds(133, 77, 93, 37);
+		ta_studentsmarks_panel.add(ta2_course_label);
+		
+		JTextPane ta2_course_pane = new JTextPane();
+		ta2_course_pane.setBackground(Color.LIGHT_GRAY);
+		ta2_course_pane.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		ta2_course_pane.setText("Course Name");
+		ta2_course_pane.setEditable(false);
+		ta2_course_pane.setBounds(402, 71, 187, 43);
+		ta_studentsmarks_panel.add(ta2_course_pane);
+		
+		JLabel ta2_studentID_label = new JLabel("Student I.D");
+		ta2_studentID_label.setForeground(Color.BLACK);
+		ta2_studentID_label.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		ta2_studentID_label.setBackground(Color.WHITE);
+		ta2_studentID_label.setBounds(133, 174, 155, 37);
+		ta_studentsmarks_panel.add(ta2_studentID_label);
+		
+		JTextPane ta2_studentID_pane = new JTextPane();
+		ta2_studentID_pane.setText("SNO");
+		ta2_studentID_pane.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		ta2_studentID_pane.setBackground(Color.WHITE);
+		ta2_studentID_pane.setBounds(402, 168, 187, 43);
+		ta_studentsmarks_panel.add(ta2_studentID_pane);
+		
+		JLabel ta2_marks_label = new JLabel("Marks Obtained");
+		ta2_marks_label.setForeground(Color.BLACK);
+		ta2_marks_label.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		ta2_marks_label.setBackground(Color.WHITE);
+		ta2_marks_label.setBounds(133, 271, 210, 37);
+		ta_studentsmarks_panel.add(ta2_marks_label);
+		
+		JTextPane ta2_marks_pane = new JTextPane();
+		ta2_marks_pane.setText("M1 or M2 or..M5, based on how you give subject numbers");
+		ta2_marks_pane.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		ta2_marks_pane.setBackground(Color.WHITE);
+		ta2_marks_pane.setBounds(402, 265, 187, 43);
+		ta_studentsmarks_panel.add(ta2_marks_pane);
+		
+		JButton ta2_add_button = new JButton("Add");
+		ta2_add_button.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		ta2_add_button.setBounds(251, 406, 186, 60);
+		ta_studentsmarks_panel.add(ta2_add_button);
+		
+		
+	}
+}
