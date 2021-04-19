272de6470d26bec01ba464311b9d26cc1c916843
shivaharshith <shiva.harshith2000@gmail.com>
2021-04-19 13:33:47 +0530

    asdasd

--- DBMS/src/Sample.java
+++ DBMS/src/Sample.java
@@ -3,5 +3,6 @@ public class Sample {
  public static void main(String args[]) {
 	 System.out.println("alkfjksdfhngjkdfjgljdfahlafgj");
 	 System.out.println("alk");
+	 System.out.println("alksadasd");
  }
 }
